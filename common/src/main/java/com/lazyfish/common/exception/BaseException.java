6d5f2583ff4b63e1b98e3f36388f979297aa8fb2
choe <348863046@qq.com>
2018-09-03 17:24:16 +0800

    框架搭建

---
+++ common/src/main/java/com/lazyfish/common/exception/BaseException.java
@@ -0,0 +1,59 @@
+package com.lazyfish.common.exception;
+
+import com.lazyfish.common.utils.StringUtil;
+
+import java.io.Serializable;
+
+/**
+ * @author cyk
+ * @date 2018/9/3/003 16:09
+ * @email choe0227@163.com
+ * @desc
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+public class BaseException extends RuntimeException implements Serializable {
+    private static final long serialVersionUID = 6812202525292547656L;
+
+    private Integer code;
+    private String logs;
+    private String describle;
+    private ErrorCode errorCode;
+
+    public  BaseException(String decrible){
+        super(decrible);
+    }
+
+    private BaseException(Integer code,String logs,String describle){
+        super(StringUtil.isEmpty(logs)?(StringUtil.isEmpty(describle)?"未知错误":describle):logs);
+    }
+
+    private BaseException(ErrorCode errorCode){
+        this(errorCode.getCode(),errorCode.getLogs(),errorCode.getDescrible());
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getLogs() {
+        return logs;
+    }
+
+    public void setLogs(String logs) {
+        this.logs = logs;
+    }
+
+    public String getDescrible() {
+        return describle;
+    }
+
+    public void setDescrible(String describle) {
+        this.describle = describle;
+    }
+}
---
+++ common/src/main/java/com/lazyfish/common/exception/ErrorCode.java
@@ -0,0 +1,50 @@
+package com.lazyfish.common.exception;
+
+/**
+ * @author cyk
+ * @date 2018/9/3/003 16:10
+ * @email choe0227@163.com
+ * @desc
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+public enum ErrorCode {
+    not_known_error(-1,"未知错误","未知错误"),
+    system_error(-9999,"系统服务异常","系统服务异常"),
+    ;
+
+    private Integer code;
+    private String describle;
+    private String logs;
+
+    ErrorCode(Integer code, String describle, String logs) {
+        this.code = code;
+        this.describle = describle;
+        this.logs = logs;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDescrible() {
+        return describle;
+    }
+
+    public void setDescrible(String describle) {
+        this.describle = describle;
+    }
+
+    public String getLogs() {
+        return logs;
+    }
+
+    public void setLogs(String logs) {
+        this.logs = logs;
+    }
+}
